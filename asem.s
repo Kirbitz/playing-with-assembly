23d4edcd0fd0625fbfd3523b26397e749e33227d
kirbitz <tyler.marefke@gmail.com>
2023-05-13 21:45:40 -0500

    updated exit number and print out second word

--- asem.s
+++ asem.s
@@ -1,3 +1,4 @@
+.section .text
 .global _start
 .intel_syntax noprefix
 
@@ -9,10 +10,20 @@ _start:
   mov rdx, 14
   syscall
 
-  // sys_exit
-  mov rax, 60
-  mov rdi, 69
+  mov rax, 1
+  mov rdi, 1
+  lea rsi, [hello_2]
+  mov rdx, 5 
   syscall
 
+  // sys_exit
+  mov rax, 60
+  mov rdi, 1 
+  syscall
+
+.section .data
 hello_world:
   .asciz "Hello, World!\n"
+
+hello_2:
+  .asciz "Test\n"
